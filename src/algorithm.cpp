0cbffe591b673e18139c06bedb7a16e5da0c6a0d
Nicolas <nicolas.martin@arobas-music.com>
2014-05-30 22:30:30 +0000

    fix issue

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -8,12 +8,26 @@ using namespace std;
 
 template<class Range, class T>
 bool contains(const Range &range, const T &value) {
-	typedef decltype(*cbegin(range)) value_type; 
-	return any_of(cbegin(range), cend(range), [&value] (const value_type &i) { return value == i;} );
+	typedef decltype(*begin(range)) value_type; 
+	return any_of(begin(range), end(range), [&value] (const value_type &i) { return value == i;} );
 }
 
-TEST(algorithm, contains) {
+TEST(algorithm, stringcontains) {
   string str("hello");
   ASSERT_TRUE(contains(str, 'o'));
+  ASSERT_FALSE(contains(str, 'O'));
+}
+
+TEST(algorithm, vectorcontains) {
+  vector<int> v {0, 1, 2, 3};
+  ASSERT_TRUE(contains(v, 0));
+  ASSERT_FALSE(contains(v, 4));
+}
+
+
+TEST(algorithm, vectorcontain) {
+  vector<int> v {0, 1, 2, 3};
+  ASSERT_TRUE(contains(v, 0));
+  ASSERT_FALSE(contains(v, 4));
 }
 
