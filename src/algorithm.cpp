f6fb0d5cb8d5d9669e934c4345c43c81e7128e69
Nicolas Martin <nicolas.martin@arobas-music.com>
2014-05-30 23:55:32 +0200

    missing capture

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -9,7 +9,7 @@ using namespace std;
 template<class Range, class T>
 bool contains(const Range &range, const T &value) {
 	typedef decltype(*cbegin(range)) value_type; 
-	return any_of(cbegin(range), cend(range), [] (const value_type &i) { return value == i;} );
+	return any_of(cbegin(range), cend(range), [&value] (const value_type &i) { return value == i;} );
 }
 
 TEST(algorithm, contains) {
