89e32992a933321668a71f48f5384d602201798b
Nicolas Martin <nicolas.martin@arobas-music.com>
2016-01-07 11:01:23 +0100

    find_range

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 template<class Range, class Value>
 bool contains(const Range &range, const Value &value) {
 	typedef decltype(*cbegin(range)) value_type; 
-	return any_of(cbegin(range), cend(range), [](const value_type &i) { return value == i;} );
+	return any_of(cbegin(range), cend(range), [&](const value_type &i) { return value == i;} );
 }
 
 TEST(any_of, contains) {
@@ -17,3 +17,9 @@ TEST(any_of, contains) {
   ASSERT_TRUE(contains(str, 'o'));
 }
 
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    auto ret = RUN_ALL_TESTS();
+    return ret;
+}
--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -1,4 +1,4 @@
-#include <gtest/gtest.h>
+﻿#include <gtest/gtest.h>
 #include <algorithm>
 #include <iterator>
 #include <string>
@@ -6,10 +6,11 @@
 
 using namespace std;
 
-template<class Range, class T>
+template <class Range, class T>
 bool contains(const Range &range, const T &value) {
-	typedef decltype(*begin(range)) value_type; 
-	return any_of(begin(range), end(range), [&value] (const value_type &i) { return value == i;} );
+  typedef decltype(*begin(range)) value_type;
+  return any_of(begin(range), end(range),
+                [&value](const value_type &i) { return value == i; });
 }
 
 TEST(algorithm, stringcontains) {
@@ -19,15 +20,54 @@ TEST(algorithm, stringcontains) {
 }
 
 TEST(algorithm, vectorcontains) {
-  vector<int> v {0, 1, 2, 3};
+  vector<int> v{0, 1, 2, 3};
   ASSERT_TRUE(contains(v, 0));
   ASSERT_FALSE(contains(v, 4));
 }
-
 
 TEST(algorithm, vectorcontain) {
-  vector<int> v {0, 1, 2, 3};
+  vector<int> v{0, 1, 2, 3};
   ASSERT_TRUE(contains(v, 0));
   ASSERT_FALSE(contains(v, 4));
 }
 
+// given a rage of values {0, 1, 2, 3} and a value
+// return the enclosing values i.e 0.5 -> {0, 1}; 5 -> {3, 3}
+template <class Range, class T>
+auto find_range(const Range &rge, const T &value)
+    -> decltype(std::make_pair(*std::begin(rge), *std::begin(rge))) {
+
+	auto begin = std::rbegin(rge);
+	auto end = std::rend(rge);
+	assert(std::distance(begin, end) >= 2);
+
+	auto lower = std::upper_bound(begin, end, value, [](const auto & l, const auto & r) {return l >= r; });
+
+	auto upper = lower;
+
+	if (lower == end) { // v < range
+		lower = upper = std::next(begin, std::distance(begin, end)-1);
+	}
+	else { // v ∈ rge, v > rge
+		upper = (lower != begin) ? std::prev(lower) : lower;
+	}
+	return std::make_pair(*lower, *upper);
+}
+
+TEST(algorithm, find_range) {
+	vector<int> values{ 0, 2, 3 };
+
+	
+	for (const auto & v : { 
+		std::make_pair(-1, std::make_pair(0, 0)),
+		std::make_pair( 0, std::make_pair(0, 2)),
+		std::make_pair( 1, std::make_pair(0, 2)),
+		std::make_pair(2, std::make_pair(2, 3)),
+		std::make_pair(3, std::make_pair(3, 3)),
+		std::make_pair( 4, std::make_pair(3, 3)),
+	}) {
+		//std::cout << v << " € [" << find_range(values, v).first << ", " << find_range(values, v).second << "]\n";
+		ASSERT_EQ(v.second, find_range(values, v.first));
+
+	}
+}
